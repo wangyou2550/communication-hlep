0ce61b0fb26e1ea4ad634ceae909a1d3e7ba5afb
wangyou2550 <1297503417@qq.com>
2023-09-02 10:56:02 +0800

    dea-1.0.0:引入路径常量

---
+++ myreqeust/PathConstant.py
@@ -0,0 +1,14 @@
+class PathConstant:
+    DNS = "http://localhost:8080/"
+    # chapter
+    GET_CHAPTER_LIST=DNS+"communication/chapter/list"
+    # section
+    GET_SECTION_CHILD=DNS+"communication/section/child/"
+    # 新增节点
+    ADD_SECTION=DNS+"communication/section"
+    # 删除节点
+    DELETE_SECTION=DNS+"communication/section"
+
+
+    # 步骤
+    ADD_STEP=DNS+"communication/step"
